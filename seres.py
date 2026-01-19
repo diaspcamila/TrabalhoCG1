d4493be9224a5394f4894fb55b362db3948e1010
letícia <157068692+Leticiaaa42@users.noreply.github.com>
2026-01-19 21:34:31 +0000

    adicionado plantas e gasto de energia

--- seres.py
+++ seres.py
@@ -1,11 +1,12 @@
 import random
+import copy
 
 class SerVivo:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.energia = 100 #energia padrão
 
-class Animal(SerVivo):
     def mover(self):
         movimentos = ['N', 'S', 'L', 'W'] #direões possíveis de andar
         passo = 10 #tamanho do passo
@@ -32,3 +33,25 @@ class Animal(SerVivo):
                 self.y += 0
                 if self.x < 0:
                     self.x += passo
+
+    def gastarEnergia(self):
+        if self.energia > 500: #gasta energia para reproduzir
+            self.energia = 100 #energia padrão
+            filho = copy.deepcopy(self)
+            filho.mover()
+            return filho
+        elif self.energia > 0: #gasta energia para se manter vivo
+            self.energia -= 1 #custo energético por turno para se manter vivo
+            return 0
+        else: #morre
+            return -1
+
+
+
+class Planta(SerVivo):
+    def fotossintese(self):
+        self.energia += 5
+
+class Animal(SerVivo):
+    def test():
+        return
