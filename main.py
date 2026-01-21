1804dd472c0866a9c46daaf3a52209be62225a6b
letícia <157068692+Leticiaaa42@users.noreply.github.com>
2026-01-21 01:46:44 +0000

    atualizada a simulação para considerar predação

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
 import sys
 import time
+import random
 from seres import *
 from graficos import *
 
 pygame.init()
-largura, altura = 800, 800
+largura, altura = 1000, 1000
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("EcoSim")
 
@@ -13,13 +14,13 @@ pygame.display.set_caption("EcoSim")
 animais = []
 plantas = []
 
-#inicializando alguns animais para teste
+#inicializando alguns animais e plantas para teste
+for i in range(10):
+    planta = Planta(random.randint(1, 39)*20, random.randint(1, 39)*20)
+    plantas.append(planta)
 for i in range(5):
-    animal = Animal(largura//2, altura//2)
+    animal = Mosquito(random.randint(1, 39)*20, random.randint(1, 39)*20)
     animais.append(animal)
-
-planta = Planta(largura//2, altura//2)
-plantas.append(planta)
 
 #loop de simulação
 rodando = True
@@ -35,28 +36,30 @@ while rodando:
     for planta in plantas:
         setMosquito(tela, planta.x, planta.y, (0, 230, 0)) #chamar função para representar graficamente
 
-        filho = planta.gastarEnergia() #gasta energia disponível para se manter vivo ou reproduzir
+        filho = planta.gastarEnergia(plantas, animais) #gasta energia disponível para se manter vivo ou reproduzir
         if filho == -1:
             plantas.remove(planta)
         elif filho != 0:
-            plantas.append(filho) #TODO plantas podem se reproduzir em cima de outras plantas
+            plantas.append(filho)
 
-        planta.fotossintese() #faz fotossintese
+        planta.fotossintese(plantas) #faz fotossintese
 
     #simular as acões de cada animal
     for animal in animais:
         setMosquito(tela, animal.x, animal.y, (0, 0, 0)) #chamar função para representar graficamente
 
-        filho = animal.gastarEnergia() #gasta energia disponível para se manter vivo ou reproduzir
+        filho = animal.gastarEnergia(plantas, animais) #gasta energia disponível para se manter vivo ou reproduzir
         if filho == -1:
             animais.remove(animal)
         elif filho != 0:
             animais.append(filho)
 
-        animal.mover()
+        animal.mover(plantas, animais)
+        animal.predar(plantas, animais)
 
     pygame.display.flip()
 
 pygame.quit()
 sys.exit()
+
 
